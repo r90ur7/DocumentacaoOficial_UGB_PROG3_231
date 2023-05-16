74f17d29e43ee23c5107f8fbada654b63790aabe
Rallenson <Rallenson900@gmail.com>
2023-05-16 03:53:47 -0300

    Adicionar arquivos de projeto.

---
+++ Relacionamentos/Data/RelacionamentosDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Relacionamentos.Models;
+
+namespace Relacionamentos.Data
+{
+    public class RelacionamentosDBContext : DbContext
+    {
+        public RelacionamentosDBContext(DbContextOptions<RelacionamentosDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+
+    }
+}
---
+++ Relacionamentos/Data/mapping/Map.cs
@@ -0,0 +1,18 @@
+﻿using Relacionamentos.Models;
+using System.Reflection.Emit;
+using Microsoft.EntityFrameworkCore;
+
+namespace Relacionamentos.Data.mapping
+{
+    public static class Map
+    {
+        public static void OnModelCreating(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Blog>()
+                .HasMany(e => e.Posts)
+                .WithOne(e => e.Blog)
+                .HasForeignKey(e => e.BlogId)
+                .HasPrincipalKey(e => e.Id);
+        }
+    }
+}
